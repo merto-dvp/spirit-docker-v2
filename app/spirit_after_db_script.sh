92298160b93ce9833d71548b62ceb116a6b6aa67
Mert Okumuş <42836922+merto-dvp@users.noreply.github.com>
2021-01-04 02:22:34 +0300

    code explanation comments

--- app/spirit_after_db_script.sh
+++ app/spirit_after_db_script.sh
@@ -1,5 +1,12 @@
+#For migrating project related changes
 sudo docker-compose run web python3 manage.py migrate
+
+#For creating admin account for project
 sudo docker-compose run web python3 manage.py createsuperuser
+
+#Folder related changes
 sudo rm db.sqlite3
 sudo mv static web
+
+#Build container for fresh start.
 sudo docker-compose up --build
