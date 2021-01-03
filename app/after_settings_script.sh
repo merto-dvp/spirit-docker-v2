68ae7aa2b424100b5e2f23335d6ee6d919b7bf3f
Mert Okumuş <42836922+merto-dvp@users.noreply.github.com>
2021-01-04 02:10:54 +0300

    code explanation comments

--- app/after_settings_script.sh
+++ app/after_settings_script.sh
@@ -1,6 +1,6 @@
 sudo rm -rf db.sqlite3 #Clean up unnecessary db files after db change
 sudo docker-compose down #Remove container for further install
-sudo docker-compose up --build# Build & install docker containers required.
+sudo docker-compose up --build  #Build & install docker containers required.
 
 
 
